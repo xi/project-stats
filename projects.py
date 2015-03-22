5bcc0da965f3715177e21408b2a537d33f1bef37
Tobias Bengfort <tobias.bengfort@gmx.net>
2015-03-22 17:14:33 +0100

    use some futures

--- projects.py
+++ projects.py
@@ -1,3 +1,7 @@
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
+
 from functools import total_ordering
 from xml.etree import ElementTree
 import argparse
